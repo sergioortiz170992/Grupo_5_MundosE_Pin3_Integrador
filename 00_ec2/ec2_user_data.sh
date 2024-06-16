10dc9d1bd0fb7dcf61dbb08854f3fb3ecf75e6af
Enrique Delgado <kikeead@gmail.com>
2024-06-15 21:27:37 -0300

    Update ec2_user_data.sh

--- 00_ec2/ec2_user_data.sh
+++ 00_ec2/ec2_user_data.sh
@@ -53,3 +53,17 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install terraform
+
+echo "Updating package lists"
+sudo apt-get update
+
+echo "Installing Nginx"
+sudo apt-get install -y nginx
+
+echo "Allowing HTTP and HTTPS traffic through the firewall"
+sudo ufw allow 'Nginx Full'
+
+echo "Ensuring Nginx is enabled and running"
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
